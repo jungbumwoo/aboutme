f6a54781b016d0e9945aa3175750b962b36e7e31
Jungbumwoo <jungbumwoo@Naver.com>
2019-12-06 17:26:37 +0900

    1206

--- app.js
+++ app.js
@@ -20,10 +20,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(__dirname + '/public'));
-app.use(express.static(__dirname + '/images'));
-app.use(express.static(path.join(__dirname, 'views')));
-app.use(express.static(path.join(__dirname, 'images')));
+
+app.use(express.static(path.join(__dirname, '/views')));
 
 
 app.use('/', indexRouter);
