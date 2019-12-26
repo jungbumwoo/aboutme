9b9d7e047f6e40bd77f959f6cf6a2e75c3da9ab3
Jungbumwoo <jungbumwoo@Naver.com>
2019-12-26 16:25:17 +0900

    1226 happy. markdown, pagine success

--- app.js
+++ app.js
@@ -31,10 +31,6 @@ app.use(express.static(path.join(__dirname, '/views')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
 
 
 //mongodb setup
@@ -50,16 +46,7 @@ db.once('open', function(){
 });
 
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
 const hljs = require("highlight.js");
 
@@ -117,5 +104,15 @@ export const mdtohtml = (i) => {
   console.log(md_to_html);
 }
 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,36 @@ import { nounlist } from "../list";
 import { mdtohtml } from "../app";
 
 
+const hljs = require("highlight.js");
+
+const md = require("markdown-it")({
+  html: false,
+  xhtmlOut: false,
+  breaks: false,
+  langPrefix: "language-",
+  linkify: true,
+  typographer: true,
+  quotes: "“”‘’",
+  highlight: function(str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return (
+          '<pre class="hljs"><code>' +
+          hljs.highlight(lang, str, true).value +
+          "</code></pre>"
+        );
+      } catch (__) {}
+    }
+
+    return (
+      '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + "</code></pre>"
+    );
+  }
+});
 
 /* GET home page. */
 
-
 /**
-
 router.get('/', function(req, res, next) {
   var page = req.param('page');
   if(page == null) {page = 1;}
@@ -34,9 +58,7 @@ router.get('/', function(req, res, next) {
 
   });
 });
-
 */
-
 /*
 router.get('/', function(req, res, next) {
   Board.find({}, function (err, board) {
@@ -44,8 +66,22 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Express', board: reverseboard});
   });
 });
-*/
-
+ */
+router.get('/', function(req, res, next) {
+  let page = Math.max(1, req.query.page);
+  let limit = 4;
+  Board.count({}, function(err, count){
+    let skip = (page -1)*limit;
+    let maxPage = Math.ceil(count/limit);
+    Board.find().skip(skip).limit(limit).exec(function (err, board){
+      if (err) return res.json({success:false, message:err});
+      let reverseboard = board.reverse();
+      res.render("index", {
+        title: 'Express', board: reverseboard, page:page, maxPage: maxPage
+      });
+    });
+  });
+});
 
 
 /*Write board page */
@@ -86,11 +122,13 @@ router.post('/board/write', function (req, res) {
 /*board find by id */
 router.get('/board/:id', function( req, res) {
   Board.findOne({_id: req.params.id}, function(err, board) {
-    const mdcontent = mdtohtml(board.contents_id);
-    res.render('board', {title: 'Board', board: board, mdcontent: mdcontent})
-    console.log(mdcontent);
-  })
-})
+    console.log(board.contents);
+    const convertedBody = md.render(board.contents); // 이 부분을 추가해주세요!
+    console.log(convertedBody);
+    res.render('board', {title: 'Board', board: board, markdowncontents: convertedBody})
+  });
+});
+
 
 /*comment insert mongo */
 router.post('/comment/write', function(req, res){
